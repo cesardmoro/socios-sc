740c2c24ba6169396abae1b555a81a75791f95ec
Cesar Moro <cmoro@genit.com.ar>
2017-10-02 23:29:48 -0300

    mensaje error festival

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -11,7 +11,7 @@ class MY_Controller extends CI_Controller {
 		$this->secured = $secured;
 		$this->require_access_permissions();
 
-		
+		 
 	}
 	public function checkSecurity (){
 		if($this->session->userdata('role')!="ADMIN"){
@@ -21,6 +21,10 @@ class MY_Controller extends CI_Controller {
 	private function require_access_permissions(){
 		$class = $this->router->fetch_class();
 		$method = $this->router->fetch_method();
+ 
+		if($class=="festival")
+			$this->session->set_flashdata('error', 'Las inscripciones solo estan disponibles para socios hasta el 17/10');      
+
 
 		if(!(strpos($method, 'public') !== false)){
 			if(!$this->session->userdata('socio')){

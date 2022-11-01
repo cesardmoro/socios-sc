59b164c44dec9288c4fde2fed6cb103a0e5a491c
Cesar Moro <cesar.d.moro@gmail.com>
2022-11-01 17:53:46 -0300

    added message to email inscripcion

--- application/views/capacitaciones/email-capacitacion-inscripcion.php
+++ application/views/capacitaciones/email-capacitacion-inscripcion.php
@@ -4,24 +4,24 @@ Tu inscripción a la capacitación <?php echo $capacitacion->titulo?> el día <?
 <br>
 <?php echo $capacitacion->email?> 
 <br>
-LA FORMA DE PAGO ES:
-Reserva de vacante $500 única manera de reservar el cupo, el resto EN EFECTIVO EL DÍA DEL CURSO.
-
-Les recordamos que solo se reserva la vacante 48 hs después de recibir el mail.
-
-Adjuntamos los Links de pago por reserva. (recargo del 6.5%)
-
-Pagá tu $500 - capacitación  con Mercado Pago: $ 532,50  https://mpago.la/23gn47B
-
-También pueden hacerlo por transferencia.
-
-Datos de la cuenta a depositar: Banco Credicoop Cooperativo Limitado, Sucursal Flores Norte (221)
-
-Titular: Asociación Civil Somos Cerveceros Cuenta corriente: 191-221-001925/0
-CBU: 1910221255022100192500
-CUIT: 30-71077118-5
-
-Por favor, luego de realizar el pago, responder con el comprobante de la operación
+LA FORMA DE PAGO ES:<br>
+Reserva de vacante $500 única manera de reservar el cupo, el resto EN EFECTIVO EL DÍA DEL CURSO.<br>
+<br>
+Les recordamos que solo se reserva la vacante 48 hs después de recibir el mail.<br>
+<br>
+Adjuntamos los Links de pago por reserva. (recargo del 6.5%)<br>
+<br>
+Pagá tu $500 - capacitación  con Mercado Pago: $ 532,50  https://mpago.la/23gn47B<br>
+<br>
+También pueden hacerlo por transferencia.<br>
+<br>
+Datos de la cuenta a depositar: Banco Credicoop Cooperativo Limitado, Sucursal Flores Norte (221)<br>
+<br>
+Titular: Asociación Civil Somos Cerveceros Cuenta corriente: 191-221-001925/0<br>
+CBU: 1910221255022100192500<br>
+CUIT: 30-71077118-5<br>
+<br>
+Por favor, luego de realizar el pago, responder con el comprobante de la operación<br>
 <br>
 Asociación Civil<br>
 Somos Cerveceros<br>
